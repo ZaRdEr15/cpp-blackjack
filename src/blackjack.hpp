3a1dc97830a0c4067e3bc45da360b3b4444757ec
AntonJ <antonjastsuk66@gmail.com>
2024-10-26 02:21:42 +0300

    Corrected the header file for classes and function declarations

--- src/blackjack.hpp
+++ src/blackjack.hpp
@@ -1,58 +1,57 @@
 #pragma once
 #include <algorithm>
 #include <random>
-#include <array>
+
+#define SUITS 4
+#define SUIT_SIZE 13
 
 namespace Blackjack {
+
+    struct Card {
+        int value;  // from 2 to 11
+        std::string face;      // number or |K|ing, |Q|ueen, |J|ack, |A|ce
+        std::string suit;      // ♣♦♥♠ (purely aesthetic)
+        bool ace;       // false by default
+        Card(const std::string f, const std::string s);
+        void faceToValue(const std::string f);
+    };
+
+    class HandHolder {
+    public:
+        std::vector<Card> hand;
+        bool finished;
+        void hit();
+        void stand();
+        virtual void calculateTotalValue();
+        virtual void showCards();
+    };
+
+    class Player : HandHolder {
+    public:
+        void showCards() override; // shows all cards
+        void doubleDown(); // increase bet by 100% and take exactly one card, then stand
+        void split();
+        void chooseAction();
+    };
+
+    class Dealer : HandHolder { // stands on 17 and higher (no matter what)
+    public:
+        void calculateTotalValue() override;
+        void showCards() override; // shows 1 card before player finished and all after finished
+    };
+
     class Game {
-        enum class Suit {Spades, Hearts, Clubs, Diamonds};
+        std::vector<Card> m_deck;
 
-        struct Card {
-            uint8_t value;  // from 2 to 11
-            char face;      // number or |K|ing, |Q|ueen, |J|ack, |A|ce
-            char suit;      // ♣♦♥♠ (purely aesthetic)
-            bool ace;       // false by default
-            Card(uint8_t v, Suit s);
-            void faceToValue(char f);
-        };
-
-        class HandHolder {
-        public:
-            std::vector<Card> hand;
-            bool finished;
-            void hit();
-            void stand();
-            virtual void calculateTotalValue();
-            virtual void showCards();
-        };
-
-        class Player : HandHolder {
-        public:
-            void showCards() override; // shows all cards
-            void doubleDown(); // increase bet by 100% and take exactly one card, then stand
-            void split();
-            void chooseAction();
-        };
-
-        class Dealer : HandHolder { // stands on 17 and higher (no matter what)
-        public:
-            void calculateTotalValue() override;
-            void showCards() override; // shows 1 card before player finished and all after finished
-        };
-
-        std::random_device m_rd;
-        std::default_random_engine m_rng;
-
-        std::array<Card, 52> m_default_deck;
-
-        Player player;
-        Dealer dealer;
+        Player m_player;
+        Dealer m_dealer;
 
         void fillDeck();
-        void shuffleCards();
+        void shuffleDeck(std::default_random_engine rng);
+        void showDeck();
         void initialDeal();
         void decideWinner();
     public:
-        static void play();
+        void play();
     };
 }
