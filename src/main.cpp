eb99b78e4dae0802cb8d20c096e5c24c5ff74ac1
Anton Jastsuk <73446410+ZaRdEr15@users.noreply.github.com>
2025-08-21 19:53:05 +0300

    Windows code for showing UTF-8 in console

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include "blackjack.hpp"
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 int main() {
+#ifdef _WIN32
+    // Set console code page to UTF-8 so console knows how to interpret string data
+    SetConsoleOutputCP(65001);
+#endif
     Blackjack::Game game;
     game.play();
 }
